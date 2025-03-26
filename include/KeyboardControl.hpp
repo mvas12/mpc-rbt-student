1d2b7eeb49d0c9fc462360140385f7840268da49
mvas12 <vasour.martin@gmail.com>
2025-03-26 14:59:49 +0100

    cv6 komit

---
+++ include/KeyboardControl.hpp
@@ -0,0 +1,19 @@
+#ifndef KEYBOARD_CONTROL
+#define KEYBOARD_CONTROL
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+#include <termios.h>
+
+class KeyboardControlNode: public rclcpp::Node {
+  public:
+    KeyboardControlNode();
+    ~KeyboardControlNode();
+  private:
+    void timerCallback();
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    struct termios old_termios_;
+};
+#endif //KEYBOARD_CONTROL
